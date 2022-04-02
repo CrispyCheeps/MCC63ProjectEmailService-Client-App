79be87783aafc66490d4514cc2b9d58759a68449
Dean Christiananda <alfadean60@gmail.com>
2022-04-02 10:14:09 +0900

    Inisiasi project email service - client app

---
+++ src/test/java/co/id/mailservice/clientapp/MailserviceClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.mailservice.clientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailserviceClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
